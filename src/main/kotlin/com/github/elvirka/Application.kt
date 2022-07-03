07e3f7f50c2acef9aa3b2c98445a71c9db6e04a5
elvirasidorova <sidorova@arrival.com>
2022-07-03 10:53:33 +0400

    Initial commit

---
+++ src/main/kotlin/com/github/elvirka/Application.kt
@@ -0,0 +1,12 @@
+package com.github.elvirka
+
+import io.ktor.server.application.*
+import com.github.elvirka.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
